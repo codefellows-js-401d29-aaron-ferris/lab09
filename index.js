37672e159f37670018c798de47a14ee2759ac3c8
abferris <abferris@gmail.com>
2019-03-28 23:21:09 -0700

    readme

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ const mongooseOptions = {
 
 /**
  * Start point for requests
- * @module /
- */mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
+ * Takes in the mongo uri the port and the mongoose options
+ */
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
 
 
 require('./src/app.js').start(process.env.PORT);
--- src/models/players/players-model.js
+++ src/models/players/players-model.js
@@ -1,12 +1,12 @@
 'use strict';
 /**
- * categories model. Defines the schema from players-schema for players and exports.
+ * players model. Defines the schema from players-schema for players and exports.
  * @module models/players/players-model.js
  */
 const Model = require('../mongo-model.js');
 const schema = require('./players-schema.js');
 /**
- * categories model. Defines the schema from base model without any extra
+ * players model. Defines the schema from base model without any extra
  * @module models/players/players-model.js
  */
 class Players extends Model {}
